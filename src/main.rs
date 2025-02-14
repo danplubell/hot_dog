1e7b1d28ab1dceaf0b768801d15ef6013ea3a325
Dan Plubell <Dan.Plubell@coxautoinc.com>
2025-02-14 16:37:45 -0600

    Update to use hooks and fetch data with use_resources

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,16 @@ fn main() {
     dioxus::launch(App);
 }
 
+#[derive(Clone)]
+pub(crate) struct TitleState(String);
+
+#[derive(serde::Deserialize)]
+struct DogApi {
+    message: String,
+}
 #[component]
 fn App() -> Element {
+    use_context_provider(|| TitleState("HotDog".to_string()));
     rsx! {
         document::Stylesheet { href: CSS }
         Title {}
@@ -19,22 +27,63 @@ fn App() -> Element {
 
 #[component]
 fn Title() -> Element {
+    let title = use_context::<TitleState>();
     rsx! {
         div { id: "title",
-            h1 { "HotDog! 🌭" }
+            h1 { "{title.0}" }
         }
     }
 }
 #[component]
 fn DogView() -> Element {
+    
+    
+    let mut img_src = use_resource(|| async move {
+        reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap()
+            .message
+    });
     rsx! {
         div { id: "dogview",
-            img { src: "https://images.dog.ceo/breeds/pitbull/dog-3981540_1280.jpg" }
+            img { src: img_src.cloned().unwrap_or_default() }
         }
         div { id: "buttons",
-            button { id: "skip", "skip" }
-            button { id: "save", "save!" }
+            button { onclick: move |_| img_src.restart(), id: "skip", "skip" }
+            button { onclick: move |_| img_src.restart(), id: "save", "save!" }
         }
 
     }
 }
+/*
+this example uses a signal for the img_src
+fn DogView() -> Element {
+    let mut img_src = use_signal(|| "".to_string());
+    
+    let skip = move |evt| {};
+    let fetch_new = move |_| async move {
+        let response = reqwest::get("https://dog.ceo/api/breeds/image/random")
+            .await
+            .unwrap()
+            .json::<DogApi>()
+            .await
+            .unwrap();
+
+        img_src.set(response.message); // fire signal
+    };
+    rsx! {
+        div { id: "dogview",
+            img { src: "{img_src}" }
+        }
+        div { id: "buttons",
+            button { onclick: skip, id: "skip", "skip" }
+            button { onclick: fetch_new, id: "save", "save!" }
+        }
+
+    }
+}
+
+ */
